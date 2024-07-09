92a6490aa7f0e567c0249e730164f69a8c4da64e
Kalkidan Asgedom <kalkidana.@Kalkidans-MacBook-Pro.local>
2024-07-09 13:39:49 -0400

    created the APIs file and worked on the openAI.py file

---
+++ app/APIs/openAi.py
@@ -0,0 +1,31 @@
+import os 
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+# Load environment variables from a .env file
+load_dotenv()
+
+
+my_api_key = os.getenv("OPENAI_KEY") #hide key later
+
+openai.api_key = my_api_key
+
+client = OpenAI(
+    api_key=my_api_key,
+)
+
+def recomendation(info):
+# Specify the model to use and the messages to send
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are giving the user a workout plan based on the information given in a weekly format where there is a specific set of workouts for each day of the week"},
+            {"role": "user", "content": f'Using this information {info}, create a workout plan for me to meet my goals'}
+        ]
+    )   
+    return completion.choices[0].message.content
+
+#if __name__ == "__main__":
+#    recomendation()
